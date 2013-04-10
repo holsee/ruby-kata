5593bcaea5dfc2730d07388fef3670a788352004
Steven Holdsworth <sholdsworth@gmail.com>
2013-04-10 13:18:52 +0100

    tabs to spaces 2

--- fizzbuzz/fizzbuzz.rb
+++ fizzbuzz/fizzbuzz.rb
@@ -1,15 +1,15 @@
 class Fizzbuzz
-	def foo n
-		fizz = n % 3 == 0
-		buzz = n % 5 == 0
-		fizzbuzz = fizz && buzz
+  def foo n
+    fizz = n % 3 == 0
+    buzz = n % 5 == 0
+    fizzbuzz = fizz && buzz
 
-		fizzbuzz ?
-			"fizzbuzz"
-		: fizz ?
-			"fizz" 
-		: buzz ?
-			"buzz"
-		: n.to_s 
-	end
+    fizzbuzz ?
+      "fizzbuzz"
+    : fizz ?
+      "fizz" 
+    : buzz ?
+      "buzz"
+    : n.to_s 
+  end
 end

8152ab62c477f7606719d81900c460f8413146d6
Steven Holdsworth <sholdsworth@gmail.com>
2013-04-10 13:17:52 +0100

    completed fizzbuzz kata -> turnaries up the ying..

--- fizzbuzz/fizzbuzz.rb
+++ fizzbuzz/fizzbuzz.rb
@@ -1,2 +1,15 @@
 class Fizzbuzz
+	def foo n
+		fizz = n % 3 == 0
+		buzz = n % 5 == 0
+		fizzbuzz = fizz && buzz
+
+		fizzbuzz ?
+			"fizzbuzz"
+		: fizz ?
+			"fizz" 
+		: buzz ?
+			"buzz"
+		: n.to_s 
+	end
 end
